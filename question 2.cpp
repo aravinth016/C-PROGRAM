a3d585bf6e2b71c461cee8ffeab686476968aeaf
aravinth016 <125422428+aravinth016@users.noreply.github.com>
2023-03-22 19:34:19 -0700

    Add files via upload

---
+++ question 2.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i=3,val;
+	val= sizeof (i)+ +f(i=1)+ +f(i-1);
+	printf("%d%d",val,i);
+	
+}
+{
+	int f(int num)
+	{
+		return num*5;
+	}
+}
---
+++ question 4.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{ 
+	const int p=5;
+	printf(" %d",p);
+}
